6e83cedd315725ab2b07a44fdea5bf62d6999c59
Peter Sheehan <petersheehan2003@gmail.com>
2024-06-27 21:56:44 +0100

    feat(plugins): adds lazy plugin manager

---
+++ lua/plugins/init.lua
@@ -0,0 +1,8 @@
+-- INFO: For now, this file exists so that if there are no other files in this
+-- directory, there technically is a plugin spec being returned (an
+-- empty table, {}). This prevents any errors when lazy tries to load plugins
+-- from this directory before I have added in any more files for each plugin.
+--
+-- This is subject to change, i.e. renaming, repurposing, deletion etc.
+
+return {}
