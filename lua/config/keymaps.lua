e55b3a1ce01482a7f03fc422001a94457a04fe35
Peter Sheehan <petersheehan2003@gmail.com>
2024-06-27 16:53:03 +0100

    feat(keymaps): add basic keymaps
    Added keymaps to:
      - clear search highlight
      - navigate splits
      - insert blank lines

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- abbreviated
+local set = vim.keymap.set
+
+-- clear search highlighting in normal mode with ESC
+set("n", "<esc>", "<cmd>nohlsearch<cr>")
+
+-- navigate splits
+set("n", "<C-h>", "<C-w><C-h>", { desc = "Focus left pane" })
+set("n", "<C-j>", "<C-w><C-j>", { desc = "Focus lower pane" })
+set("n", "<C-k>", "<C-w><C-k>", { desc = "Focus upper pane" })
+set("n", "<C-l>", "<C-w><C-l>", { desc = "Focus right pane" })
+set("n", "<leader>-", "<C-W>s", { desc = "Split below", remap = true })
+set("n", "<leader>|", "<C-W>v", { desc = "Split right", remap = true })
+
+-- insert blank lines
+set("n", "<leader>o", "o<esc>", { desc = "Insert line below" })
+set("n", "<leader>O", "O<esc>", { desc = "Insert line above" })
