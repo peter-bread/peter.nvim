bfa8e06c9b08ac2d25772b42f211619a00cd2ed0
Peter Sheehan <petersheehan2003@gmail.com>
2024-06-27 22:02:42 +0100

    feat(plugins): adds rose-pine colorscheme

--- init.lua
+++ init.lua
@@ -1,1 +1,3 @@
 require("config")
+
+pcall(function() vim.cmd([[colorscheme rose-pine-main]]) end)
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  -- add more colorschemes here (using `event = "VeryLazy"` and `lazy = true`)
+}
