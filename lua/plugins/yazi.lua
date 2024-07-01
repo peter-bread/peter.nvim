aecbd0790eddee8a4fa5970757bf93e77ec0c2e5
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-01 17:36:48 +0100

    feat(plugins): adds yazi (file explorer)

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,27 @@
+return {
+  "mikavilpas/yazi.nvim",
+  lazy = true,
+  keys = {
+    {
+      "<leader>y",
+      function()
+        require("yazi").yazi()
+      end,
+      desc = "yazi",
+    },
+    {
+      "<leader>Y",
+      function()
+        require("yazi").yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "yazi (cwd)",
+    },
+  },
+  ---@type YaziConfig
+  opts = {
+    open_for_directories = true,
+  },
+  config = function(_, opts)
+    require("yazi").setup(opts)
+  end
+}
