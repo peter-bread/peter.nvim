49ad8c844644ea47dcd31ae239231bc36f53f523
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-03 19:36:01 +0100

    feat(plugins): adds mini.pairs

---
+++ lua/plugins/pairs.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.pairs",
+  event = { "InsertEnter", "CmdlineEnter" },
+  opts = {
+    modes = {
+      insert = true,
+      command = true,
+      terminal = false,
+    },
+    markdown = true,
+  },
+}
