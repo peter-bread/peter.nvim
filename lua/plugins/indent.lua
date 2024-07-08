368af0686f949c53f709660013d56854d00ec20d
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-08 22:41:10 +0100

    feat(indent): use `mini.indentscope` and `indent-blankline.nvim`
    `indent-blankline.nvim` for indent guide
    `mini.indentscope` for indent scope

---
+++ lua/plugins/indent.lua
@@ -0,0 +1,41 @@
+return {
+
+  -- indentation guides
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    lazy = true,
+    event = { "BufReadPost", "BufNewFile" },
+    main = "ibl",
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = {
+        enabled = false,
+      }
+    },
+  },
+
+  -- scope
+  {
+    "echasnovski/mini.indentscope",
+    lazy = true,
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      require("mini.indentscope").setup({
+        draw = {
+          delay = 0,
+          animation = require("mini.indentscope").gen_animation.none(), -- 1*
+        },
+        symbol = "│",
+      })
+    end,
+  },
+}
+
+-- 1* Since the module is required during its own setup, it must be done in
+--    the config function as it runs when the plugin is loaded.
+--
+--    (If it were in an external `opts` table, it would attempt to execute
+--    before the plugin had been loaded so the module would not yet exist).
