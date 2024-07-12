4be71589e7234b8810a5316f5a970ed8cf23d399
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-12 21:24:41 +0100

    refactor(lualine): change `opt` from a table to a function

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -12,40 +12,43 @@ return {
       vim.o.laststatus = 0
     end
   end,
-  opts = {
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_x = {
+  opts = function()
+    local opts = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_x = {
 
-        -- no. of lazy package updates
-        {
-          require("lazy.status").updates,
-          cond = require("lazy.status").has_updates,
-          color = { fg = "#ff9e64" },
-        },
-
-        -- git diff
-        {
-          "diff",
-          symbols = {
-            -- nf-oct-diff-
-            added = " ",
-            removed = " ",
-            modified = " ",
+          -- no. of lazy package updates
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = "#ff9e64" },
           },
-          source = function()
-            local gs = vim.b.gitsigns_status_dict
-            if gs then
-              return {
-                added = gs.added,
-                removed = gs.removed,
-                modified = gs.changed,
-              }
-            end
-          end,
+
+          -- git diff
+          {
+            "diff",
+            symbols = {
+              -- nf-oct-diff-
+              added = " ",
+              removed = " ",
+              modified = " ",
+            },
+            source = function()
+              local gs = vim.b.gitsigns_status_dict
+              if gs then
+                return {
+                  added = gs.added,
+                  removed = gs.removed,
+                  modified = gs.changed,
+                }
+              end
+            end,
+          },
         },
-      },
-    },
-  },
+      }
+    }
+    return opts
+  end,
 }
