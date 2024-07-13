87d33eac7e7120d74080e5dd9e15c8102e6e9271
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-13 19:01:20 +0100

    feat(rose-pine): borderless telescope

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,7 +4,24 @@ return {
     name = "rose-pine",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      variant = "auto",
+      dark_variant = "main",
+      highlight_groups = {
+        TelescopeBorder = { fg = "overlay", bg = "overlay" },
+        TelescopeNormal = { fg = "subtle", bg = "overlay" },
+        TelescopeSelection = { fg = "text", bg = "highlight_med" },
+        TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+        TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+
+        TelescopeTitle = { fg = "base", bg = "love" },
+        TelescopePromptTitle = { fg = "base", bg = "pine" },
+        TelescopePreviewTitle = { fg = "base", bg = "iris" },
+
+        TelescopePromptNormal = { fg = "text", bg = "surface" },
+        TelescopePromptBorder = { fg = "surface", bg = "surface" },
+      },
+    },
   },
   -- add more colorschemes here (using `event = "VeryLazy"` and `lazy = true`)
 }
