ed9483ae0846d8d21051d7567f0f552c65c9db62
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-21 23:34:56 +0100

    refactor(pairs): replace `mini.pairs` with `nvim-autopairs`
    `mini.pairs` had undesired behaviour when closing some pairs.
    `nvim-autopairs` does not have this issue.

---
+++ lua/plugins/pairs.lua
@@ -0,0 +1,13 @@
+return {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  opts = {
+    check_ts = true,
+  },
+  config = function(_, opts)
+    require("nvim-autopairs").setup(opts)
+    -- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    -- local cmp = require("cmp")
+    -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
