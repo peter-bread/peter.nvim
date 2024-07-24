2a673fde95d7886d5cdf3cf7611f77a99e7f6a60
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-24 21:35:09 +0100

    feat(cmp): update insert keymaps

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -66,17 +66,18 @@ return {
       },
       completion = { completeopt = "menu,menuone,noinsert" },
       mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item({
-          behavior = cmp.SelectBehavior.Insert,
-        }),
-        ["<C-p>"] = cmp.mapping.select_prev_item({
-          behavior = cmp.SelectBehavior.Insert,
-        }),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-n>"] = cmp.mapping.select_next_item({
+          behavior = cmp.SelectBehavior.Select,
+        }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({
+          behavior = cmp.SelectBehavior.Select,
+        }),
         ["<C-y>"] = cmp.mapping.confirm({
-          behavior = cmp.SelectBehavior.Insert,
+          behavior = cmp.SelectBehavior.Select,
           select = true,
         }),
       }),
