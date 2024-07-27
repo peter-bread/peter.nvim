296155724c468d4aa527878c4a5ad0480e6af598
Peter Sheehan <petersheehan2003@gmail.com>
2024-07-27 15:25:16 +0100

    feat(kanagawa): enable borderless telescope

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -26,11 +26,25 @@ return {
       },
     },
   },
-  -- add more colorschemes here (using `event = "VeryLazy"` and `lazy = true`)
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          -- stylua: ignore start
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+          -- stylua: ignore end
+        }
+      end,
+    },
   },
 }
