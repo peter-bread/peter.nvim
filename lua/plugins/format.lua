e130ff8eb1d7e427fb1649b4a2104ad4fe8a4f41
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-05 21:27:41 +0100

    feat(format): add injected formatting keymap and move settings to defaults

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -3,14 +3,33 @@ return {
   event = { "BufWritePre" },
   cmd = { "ConformInfo" },
   keys = {
-    -- stylua: ignore
-    { "<leader>cf", function() require("conform").format({ async = true }) end, mode = "", desc = "Format Buffer" },
+    {
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format Buffer",
+    },
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({
+          formatters = { "injected" },
+          timeout_ms = 3000,
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
   },
   ---@type conform.setupOpts
   opts = {
-    format_on_save = {
-      timeout_ms = 5000,
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false,
       lsp_format = "fallback",
     },
+    format_on_save = {},
   },
 }
