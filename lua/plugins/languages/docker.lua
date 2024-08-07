0d2118d9364a9564889d590034a07be67d69cc73
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-07 21:13:18 +0100

    feat(languages): add docker

---
+++ lua/plugins/languages/docker.lua
@@ -0,0 +1,26 @@
+local L = require("util.new_lang")
+
+return {
+  L.treesitter({
+    "dockerfile",
+  }),
+
+  L.mason({
+    "dockerls", -- lsp
+    "docker_compose_language_service", -- lsp
+    "hadolint", -- linter
+  }),
+
+  L.lspconfig({
+    servers = {
+      dockerls = {},
+      docker_compose_language_service = {},
+    },
+  }),
+
+  L.lint({
+    linters_by_ft = {
+      dockerfile = { "hadolint" },
+    },
+  }),
+}
