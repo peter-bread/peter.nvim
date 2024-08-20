35c709e9298a80316aba18851b37aea79abadc12
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-20 15:30:09 +0100

    feat(languages): add elixir

---
+++ lua/plugins/languages/elixir.lua
@@ -0,0 +1,58 @@
+local L = require("util.new_lang")
+
+return {
+  L.treesitter({
+    "elixir",
+    "heex",
+    "eex",
+  }),
+
+  L.mason({
+    "elixirls",
+  }),
+
+  L.lspconfig({
+    servers = {
+      elixirls = {
+        experimental = {
+          completions = {
+            enable = true,
+          },
+        },
+      },
+    },
+  }),
+
+  L.test({
+    dep = "jfpedroza/neotest-elixir",
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      adapters = {
+        ["neotest-elixir"] = {},
+      },
+    },
+  }),
+
+  L.dap({
+    adapters = {
+      mix_task = function()
+        return {
+          type = "executable",
+          command = vim.fn.exepath("elixir-ls-debugger"),
+          args = {},
+        }
+      end,
+    },
+    configurations = {
+      elixir = {
+        {
+          type = "mix_task",
+          name = "mix task",
+          task = "test",
+          request = "launch",
+          projectDir = "${workspaceFolder}",
+        },
+      },
+    },
+  }),
+}
