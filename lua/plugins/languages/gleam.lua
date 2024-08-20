a7296be1b3c7bd159c6f2b16f19457f92790522f
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-20 15:25:47 +0100

    feat(languages): add gleam

---
+++ lua/plugins/languages/gleam.lua
@@ -0,0 +1,19 @@
+local L = require("util.new_lang")
+
+return {
+  L.treesitter({
+    "gleam",
+  }),
+
+  L.lspconfig({
+    servers = {
+      gleam = {},
+    },
+  }),
+
+  L.format({
+    formatters_by_ft = {
+      gleam = { "gleam" },
+    },
+  }),
+}
