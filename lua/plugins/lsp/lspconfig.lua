c0d981753084df53aec13897c6993f44d4e9d516
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-29 22:24:48 +0100

    refactor(lsp): redo inlay hint setup

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -22,39 +22,28 @@ return {
     "williamboman/mason-lspconfig.nvim",
     "hrsh7th/cmp-nvim-lsp",
   },
-  opts = function()
-    local ret = {
-      -- options for vim.diagnostic.config()
-      diagnostics = {
-        underline = true,
-        update_in_insert = false,
-        virtual_text = {
-          spacing = 4,
-          source = "if_many",
-          prefix = "●",
-        },
-        severity_sort = true,
+  opts = {
+    diagnostics = {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = {
+        spacing = 4,
+        source = "if_many",
+        prefix = "●",
       },
-      capabilities = {
-        workspace = {
-          fileOperations = {
-            didRename = true,
-            willRename = true,
-          },
+      severity_sort = true,
+    },
+    capabilities = {
+      workspace = {
+        fileOperations = {
+          didRename = true,
+          willRename = true,
         },
       },
-      inlay_hints = {
-        enabled = true,
-        exclude = {},
-      },
-      codelens = {
-        enabled = true,
-      },
-      ---@type CustomLspSetup
-      setup = {},
-    }
-    return ret
-  end,
+    },
+    ---@type CustomLspSetup
+    setup = {},
+  },
   config = function(_, opts)
     require("mason-lspconfig").setup()
 
@@ -64,57 +53,10 @@ return {
       lsp.create_lsp_keymaps(client, bufnr)
     end)
 
-    if opts.inlay_hints.enabled then
-      ---@diagnostic disable-next-line: unused-local
-      lsp.on_supports_method("textDocument/inlayHint", function(client, bufnr)
-        if
-          vim.api.nvim_buf_is_valid(bufnr)
-          and vim.bo[bufnr].buftype == ""
-          and not vim.tbl_contains(
-            opts.inlay_hints.exclude,
-            vim.bo[bufnr].filetype
-          )
-        then
-          vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-
-          ---Toggle inlay hints.
-          ---@param global boolean True: all buffers. False: current buffer.
-          local function toggle_inlay_hints(global)
-            -- Determine the buffer number: `nil` for global, `0` for current buffer
-            local buf
-
-            if global then
-              buf = nil
-            else
-              buf = 0
-            end
-
-            -- Toggle LSP inlay hints
-            vim.lsp.inlay_hint.enable(
-              not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }),
-              { bufnr = buf }
-            )
-
-            -- Attempt to toggle clangd_extensions inlay hints
-            local ok, inlay_hints =
-              pcall(require, "clangd_extensions.inlay_hints")
-            if ok then
-              inlay_hints.toggle_inlay_hints()
-            end
-          end
-
-          -- Toggle inlay hints (global)
-          vim.keymap.set("n", "<leader>uh", function()
-            toggle_inlay_hints(true)
-          end, { desc = "Toggle Inlay Hints", buffer = bufnr })
-
-          -- Toggle inlay hints (current buffer)
-          vim.keymap.set("n", "<leader>uH", function()
-            toggle_inlay_hints(false)
-          end, { desc = "Toggle Inlay Hints (Buf)", buffer = bufnr })
-        end
-      end)
-    end
+    ---@diagnostic disable-next-line: unused-local
+    lsp.on_supports_method("textDocument/inlayHint", function(client, bufnr)
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end)
 
     ---@diagnostic disable-next-line: unused-local
     lsp.on_supports_method("textDocument/codeLens", function(client, bufnr)
