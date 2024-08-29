b710a8f9d035427ecd6356b2a883a06ae9ae9be1
Peter Sheehan <petersheehan2003@gmail.com>
2024-08-29 22:16:22 +0100

    refactor(lsp): remove `["*"]` setup option

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -2,7 +2,6 @@
 ---Return `true` if LSP setup is complete, i.e. `require("lspconfig")[server_name].setup(server_opts)` is not needed.
 ---Return `false|nil` if LSP setup is not complete, i.e. extra config is done in this function but `require("lspconfig")[server_name].setup(server_opts)` still needs to be used to finish setup.
 ---Use ["server_name"] for server-specific setups.
----Use ["*"] for fallback setup for any server that doesn't have its own custom setup.
 ---e.g.
 --- ```lua
 --- setup = {
@@ -10,11 +9,6 @@
 ---     -- custom config
 ---     require("util.lsp").on_attach(...)
 ---     return false -- still need to use lspconfig
----   end,
----   ["*"] = function(server, opts)
----     -- maybe modify opts
----     require("lspconfig")[server].setup(opts)
----     return true -- LSP already set up
 ---   end,
 --- },
 --- ```
@@ -171,14 +165,10 @@ return {
       )
 
       -- if there is a custom setup function, run it
-      -- if it returns false, then it is just doing additional config. LSP will still be setup via standard `lspconfig`. <11j>
-      -- if it returns true, then it is setting up the LSP itself, not using the standard `lspconfig` as in this function. <10j>
+      -- if it returns false, then it is just doing additional config. LSP will still be setup via standard `lspconfig`.
+      -- if it returns true, then it is setting up the LSP itself, not using the standard `lspconfig` as in this function.
       if opts.setup[server_name] then
         if opts.setup[server_name](server_name, server_opts) then
-          return
-        end
-      elseif opts.setup["*"] then
-        if opts.setup["*"](server_name, server_opts) then
           return
         end
       end
