5dc733b85ef17a69a1ce9e5bb2595b8d14e86769
Peter Sheehan <petersheehan2003@gmail.com>
2024-09-25 19:12:55 +0100

    feat(git): add git filetypes so treesitter works in dotfiles repo

--- lua/plugins/languages/git.lua
+++ lua/plugins/languages/git.lua
@@ -1,5 +1,12 @@
 local L = require("util.new_lang")
 
+vim.filetype.add({
+  pattern = {
+    ["${DOTFILES}/git/.*gitconfig.*"] = "gitconfig",
+    ["${DOTFILES}/git/.*gitignore.*"] = "gitignore",
+  },
+})
+
 return {
   L.treesitter({
     "git_config",
