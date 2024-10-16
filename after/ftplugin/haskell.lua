aaa7e958fbb77e01a0cab1c4aa8d5a4cae91ae3f
Peter Sheehan <petersheehan2003@gmail.com>
2024-10-16 22:40:22 +0100

    feat(haskell): update keymaps and move them to localleader

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -1,19 +1,29 @@
 local ht = require("haskell-tools")
 local bufnr = vim.api.nvim_get_current_buf()
-local lsp = require("util.lsp")
 
-vim.keymap.set("n", "<leader>sh", ht.hoogle.hoogle_signature, {
-  desc = "Search Hoogle",
-  buffer = bufnr,
-  noremap = true,
-  silent = true,
-})
-
-lsp.on_attach(function(client, buf)
-  vim.keymap.set("n", "<leader>cA", ht.lsp.buf_eval_all, {
-    desc = "LSP Run All Codelens",
-    buffer = buf,
+local set = vim.keymap.set
+local opts = function(desc)
+  return {
+    desc = desc,
+    buffer = bufnr,
     noremap = true,
     silent = true,
-  })
-end, "haskell-tools.nvim")
+  }
+end
+
+set("n", "<localleader>s", ht.hoogle.hoogle_signature, opts("Search Hoogle"))
+
+set("n", "<localleader>c", ht.lsp.buf_eval_all, opts("LSP Run All Codelens"))
+
+set("n", "<localleader>r", ht.repl.toggle, opts("Toggle GHCi repl (pkg)"))
+
+set("n", "<localleader>R", function()
+  ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+end, opts("Toggle GHCi repl (buf)"))
+
+set("n", "<localleader>q", ht.repl.quit, opts("Quit GHCi repl"))
+
+-- HLS
+set("n", "<localleader>s", ht.lsp.start, opts("HLS Start"))
+set("n", "<localleader>S", ht.lsp.stop, opts("HLS Stop"))
+set("n", "<localleader>e", ht.lsp.buf_eval_all, opts("HLS Eval All"))
