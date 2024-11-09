690bac69df1bc49c0b7f7bf4c58b792508b6dd0b
Peter Sheehan <petersheehan2003@gmail.com>
2024-11-09 00:29:15 +0000

    perf: lazy load overseer

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,4 +1,9 @@
 return {
   "stevearc/overseer.nvim",
+  cmd = { "OverseerRun" },
+  keys = {
+    -- TODO: keymaps. need to decide what prefix to use since o and O are taken
+    -- right now.
+  },
   opts = {},
 }
