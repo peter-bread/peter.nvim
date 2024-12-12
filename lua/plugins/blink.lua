35f760af5816bdb3ca3cac6e0c05b625eb9d946c
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-12 23:25:02 +0000

    feat(blink): update menu

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -69,8 +69,23 @@ return {
       },
 
       menu = {
-        min_width = 40,
         winblend = vim.o.pumblend,
+        padding = 2,
+        draw = {
+          treesitter = true,
+          columns = {
+            { "label", "label_description", gap = 1 },
+            { "kind_icon", "kind", gap = 1 },
+            { "source_name" },
+          },
+          components = {
+            source_name = {
+              text = function(ctx)
+                return "[" .. ctx.source_name .. "]"
+              end,
+            },
+          },
+        },
       },
 
       documentation = {
