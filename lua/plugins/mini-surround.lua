c702dd4057e507e1c334a9c71d8f5704a71aaa61
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-20 14:11:37 +0000

    refactor(mini.surround): remove reliance on `util.lazy`
    Closes #48.

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -1,34 +1,27 @@
+local mappings = {
+  add = "gsa", -- Add surrounding in Normal and Visual modes
+  delete = "gsd", -- Delete surrounding
+  find = "gsf", -- Find surrounding (to the right)
+  find_left = "gsF", -- Find surrounding (to the left)
+  highlight = "gsh", -- Highlight surrounding
+  replace = "gsr", -- Replace surrounding
+  update_n_lines = "gsn", -- Update `n_lines`
+}
+
 return {
   {
     "echasnovski/mini.surround",
-    keys = function(_, keys)
-      -- Populate the keys based on the user's options
-      local opts = require("util.lazy").opts("mini.surround")
-      -- stylua: ignore
-      local mappings = {
-        { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "v" } },
-        { opts.mappings.delete, desc = "Delete Surrounding" },
-        { opts.mappings.find, desc = "Find Right Surrounding" },
-        { opts.mappings.find_left, desc = "Find Left Surrounding" },
-        { opts.mappings.highlight, desc = "Highlight Surrounding" },
-        { opts.mappings.replace, desc = "Replace Surrounding" },
-        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-      }
-      mappings = vim.tbl_filter(function(m)
-        return m[1] and #m[1] > 0
-      end, mappings)
-      return vim.list_extend(mappings, keys)
-    end,
+    keys = {
+      { mappings.add, desc = "Add Surrounding", mode = { "n", "v" } },
+      { mappings.delete, desc = "Delete Surrounding" },
+      { mappings.find, desc = "Find Right Surrounding" },
+      { mappings.find_left, desc = "Find Left Surrounding" },
+      { mappings.highlight, desc = "Highlight Surrounding" },
+      { mappings.replace, desc = "Replace Surrounding" },
+      { mappings.update_n_lines, desc = "Update `config.n_lines`" },
+    },
     opts = {
-      mappings = {
-        add = "gsa", -- Add surrounding in Normal and Visual modes
-        delete = "gsd", -- Delete surrounding
-        find = "gsf", -- Find surrounding (to the right)
-        find_left = "gsF", -- Find surrounding (to the left)
-        highlight = "gsh", -- Highlight surrounding
-        replace = "gsr", -- Replace surrounding
-        update_n_lines = "gsn", -- Update `n_lines`
-      },
+      mappings = mappings,
     },
   },
 }
