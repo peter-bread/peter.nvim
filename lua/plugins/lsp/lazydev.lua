d84bf6c6f9a652bce9c705e935265c45155ba3d3
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-20 19:02:12 +0000

    fix(blink): add lazydev source correctly (I think)

--- lua/plugins/lsp/lazydev.lua
+++ lua/plugins/lsp/lazydev.lua
@@ -2,29 +2,43 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
+
+    ---@module "lazydev"
+    ---@type lazydev.Config
     opts = {
       library = {
+        "lazy.nvim",
         { path = "luvit-meta/library", words = { "vim%.uv" } },
         { path = "snacks.nvim", words = { "Snacks" } },
+      },
+      integrations = {
+        lspconfig = true,
+        cmp = false,
+        coq = false,
       },
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
   {
     "saghen/blink.cmp",
-    opts = {
-      sources = {
-        providers = {
- --         lsp = { fallback_for = { "lazydev" } },
-  --        lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+
+    ---@module "blink.cmp"
+
+    ---Extend `opts` table
+    ---@param _ any
+    ---@param opts blink.cmp.Config
+    opts = function(_, opts)
+      opts.sources.default =
+        vim.list_extend({ "lazydev" }, opts.sources.default or {})
+
+      opts.sources.providers =
+        vim.tbl_extend("force", opts.sources.providers or {}, {
           lazydev = {
             name = "LazyDev",
             module = "lazydev.integrations.blink",
             score_offset = 100,
           },
-        },
-      },
-      -- opts_extend = { "lazydev" },
-    },
+        })
+    end,
   },
 }
