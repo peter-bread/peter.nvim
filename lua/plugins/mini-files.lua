51224d0e8918c2ab19717f6bedd4e2e986fdd342
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-20 23:36:43 +0000

    feat(mini.files): use solid border

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -29,6 +29,23 @@ return {
     },
     config = function(_, opts)
       require("mini.files").setup(opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesWindowOpen",
+        group = vim.api.nvim_create_augroup("UserMiniFiles", { clear = true }),
+
+        callback = function(args)
+          local win_id = args.data.win_id
+
+          -- Customise window-local settings
+          vim.wo[win_id].winblend = 10
+
+          local config = vim.api.nvim_win_get_config(win_id)
+          config.border = "solid"
+
+          vim.api.nvim_win_set_config(win_id, config)
+        end,
+      })
     end,
   },
 }
