103164d88fdd02e666e1130530aa2b10814e97a2
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-21 19:26:21 +0000

    feat(snippets): load code-snippets files from vscode directory
    Closes #47

--- lua/peter/plugins/snippets.lua
+++ lua/peter/plugins/snippets.lua
@@ -11,19 +11,52 @@ return {
     version = "v2.*",
     lazy = true,
     config = function()
+      local loader = require("luasnip.loaders.from_vscode")
+
       -- load all snippets defined in snippets directory in neovim config
-      require("luasnip.loaders.from_vscode").lazy_load({
-        paths = "./snippets",
-      })
+      loader.lazy_load({ paths = "./snippets" })
 
-      -- TODO: find project root, check for .vscode, iterate through and load
-      -- any *.code-snippet files dynamically
+      -- Project root directory
+      local root = vim.fs.root(0, { ".git", ".vscode", ".github" })
 
-      -- load project-local snippets
-      -- right now, the file must *ALWAYS* be named `project.code-snippets`
-      require("luasnip.loaders.from_vscode").load_standalone({
-        path = ".vscode/project.code-snippets",
-      })
+      if not root then
+        return
+      end
+
+      -- vscode directory
+      local vscode =
+        vim.fs.find(".vscode", { type = "directory", path = root })[1]
+
+      if not vscode then
+        return
+      end
+
+      -- get relative paths to each `*.code-snippets` files
+      local handle = vim.uv.fs_scandir(vscode)
+
+      if not handle then
+        return
+      end
+
+      -- relative paths to *.code-snippets files
+      local snippets = {}
+
+      while true do
+        local name, type = vim.uv.fs_scandir_next(handle)
+        if not name then
+          break
+        end
+        if type == "file" and name:match("%.code%-snippets$") then
+          table.insert(snippets, ".vscode/" .. name)
+        end
+      end
+
+      -- load snippets
+      if #snippets > 0 then
+        for _, path in ipairs(snippets) do
+          loader.load_standalone({ path = path })
+        end
+      end
     end,
   },
 }
