5e127121cc279f4a2f56d4bc8fd2f2725465b583
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-22 19:13:26 +0000

    fix(go): remove `fieldalignment` analyzer since it has been deprecated
    See https://github.com/golang/go/issues/67762

--- lua/peter/plugins/languages/go.lua
+++ lua/peter/plugins/languages/go.lua
@@ -43,7 +43,6 @@ return {
               rangeVariableTypes = true,
             },
             analyses = {
-              fieldalignment = true,
               nilness = true,
               unusedparams = true,
               unusedwrite = true,
