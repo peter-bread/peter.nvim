5928c924acab730c31c41e2b511c38f6d21510c2
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-27 21:00:09 +0000

    chore(mini.files): remove old code and replace with comment to help pagfe

--- lua/peter/plugins/mini-files.lua
+++ lua/peter/plugins/mini-files.lua
@@ -52,22 +52,8 @@ return {
         { fg = modified_hl_link.fg, bg = border_hl_link.bg }
       )
 
-      -- vim.api.nvim_create_autocmd("User", {
-      --   pattern = "MiniFilesWindowOpen",
-      --   group = vim.api.nvim_create_augroup("UserMiniFiles", { clear = true }),
-      --
-      --   callback = function(args)
-      --     local win_id = args.data.win_id
-      --
-      --     -- Customise window-local settings
-      --     vim.wo[win_id].winblend = 10
-      --
-      --     local config = vim.api.nvim_win_get_config(win_id)
-      --     -- config.border = "solid"
-      --
-      --     vim.api.nvim_win_set_config(win_id, config)
-      --   end,
-      -- })
+      -- For other window customisation,
+      -- See :h MiniFiles.examples  # Customize windows
     end,
   },
 }
