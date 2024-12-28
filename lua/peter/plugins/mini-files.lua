bc352ccc5341163c5e922251ade28ccef116addf
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-28 20:50:38 +0000

    feat(mini.files): go back to default borders and hl groups

--- lua/peter/plugins/mini-files.lua
+++ lua/peter/plugins/mini-files.lua
@@ -29,31 +29,6 @@ return {
     },
     config = function(_, opts)
       require("mini.files").setup(opts)
-
-      local border = "MiniFilesBorder"
-      local modified = "MiniFilesBorderModified"
-
-      local border_hl = vim.api.nvim_get_hl(0, { name = border })
-      local border_hl_link = vim.api.nvim_get_hl(0, { name = border_hl.link })
-
-      local modified_hl = vim.api.nvim_get_hl(0, { name = modified })
-      local modified_hl_link =
-        vim.api.nvim_get_hl(0, { name = modified_hl.link })
-
-      vim.api.nvim_set_hl(
-        0,
-        border,
-        { fg = border_hl_link.bg, bg = border_hl_link.bg }
-      )
-
-      vim.api.nvim_set_hl(
-        0,
-        modified,
-        { fg = modified_hl_link.fg, bg = border_hl_link.bg }
-      )
-
-      -- For other window customisation,
-      -- See :h MiniFiles.examples  # Customize windows
     end,
   },
 }
