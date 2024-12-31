6f62911918b4840808ea5c577be9ffc72d50ea33
Peter Sheehan <petersheehan2003@gmail.com>
2024-12-31 20:26:18 +0000

    feat(mini.icons): add new icons and reformat

--- lua/peter/plugins/mini-icons.lua
+++ lua/peter/plugins/mini-icons.lua
@@ -5,27 +5,67 @@ return {
     -- no event required. will be loaded when another plugin needs it
     opts = {
       style = "glyph",
+
       --fallbacks for any other category
       default = {},
+
       -- directory path (basename only)
       directory = {
-        ["config"] = { glyph = "" },
-        ["plugin"] = { glyph = "" },
-        ["plugins"] = { glyph = "" },
+        -- stylua: ignore start
+        ["config"]    = { glyph = "" },
+        ["plugin"]    = { glyph = "" },
+        ["plugins"]   = { glyph = "" },
         ["workflows"] = { glyph = "󰄴" },
+        ["util"]      = { glyph = "󱧼" },
+        ["utils"]     = { glyph = "󱧼" },
+        -- stylua: ignore end
       },
+
       -- extensions w/o dot prefix
       extension = {},
+
       -- file path (basename only)
-      file = {},
+      file = {
+        -- stylua: ignore start
+        [".ignore"]                 = { glyph = "", hl = "MiniIconsRed" },
+        [".gitignore"]              = { hl = "MiniIconsRed" },
+        [".prettierignore"]         = { glyph = "", hl = "MiniIconsRed" },
+        [".prettierrc"]             = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.json"]        = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.yml"]         = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.yaml"]        = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.json5"]       = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.js"]          = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.config.js"]   = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.mjs"]         = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.config.mjs"]  = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.cjs"]         = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.config.cjs"]  = { glyph = "", hl = "MiniIconsGrey" },
+        [".prettierrc.toml"]        = { glyph = "", hl = "MiniIconsGrey" },
+        ["lazy-lock.json"]          = { glyph = "󰒲" },
+        [".nvim.lua"]               = { glyph = "", hl = "MiniIconsGreen" },
+        ["README.md"]               = { glyph = "󰋼", hl = "MiniIconsBlue" },
+        -- stylua: ignore end
+      },
+
       -- filetype
       filetype = {
-        lua = { hl = "MiniIconsBlue" },
+        -- stylua: ignore start
+        lua       = { hl = "MiniIconsBlue" },
+        markdown  = { hl = "MiniIconsBlue" },
+        -- stylua: ignore end
       },
+
       -- "LSP kind" values
       lsp = {},
+
       -- operating system
       os = {},
+
+      -- default value right now
+      use_file_extensions = function(ext, file)
+        return true
+      end,
     },
     init = function()
       package.preload["nvim-web-devicons"] = function()
