d05b4b8d033f24163e17c461c3686a8a3c1c088d
Peter Sheehan <petersheehan2003@gmail.com>
2025-03-15 23:42:09 +0000

    feat(languages): add prose

---
+++ lua/peter/plugins/languages/prose.lua
@@ -0,0 +1,44 @@
+local L = require("peter.util.new_lang")
+
+local files = require("peter.util.files")
+
+local userDictPath = files.safe_concat_path({
+  vim.fn.getenv("XDG_DATA_HOME"),
+  "harper-ls",
+  "dictionary.txt",
+})
+
+local fileDictPath = files.safe_concat_path({
+  vim.fn.getenv("XDG_DATA_HOME"),
+  "harper-ls",
+  "file_dictionaries",
+})
+
+return {
+  L.mason({
+    "harper_ls",
+  }),
+
+  L.lspconfig({
+    servers = {
+      harper_ls = {
+        settings = {
+          ["harper-ls"] = {
+            userDictPath = userDictPath,
+            fileDictPath = fileDictPath,
+
+            diagnosticSeverity = "hint",
+
+            -- cut-and-paste config for fewer false positives when programming
+            linters = {
+              SentenceCapitalization = false,
+              SpellCheck = false,
+            },
+
+            markdown = { IgnoreLinkTitle = true },
+          },
+        },
+      },
+    },
+  }),
+}
