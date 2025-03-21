f2c997a2e9b2251567213d84578b49f0fae6d262
Peter Sheehan <petersheehan2003@gmail.com>
2025-03-21 15:29:35 +0000

    feat(pairs): disabled in snacks picker by default

--- lua/peter/plugins/pairs.lua
+++ lua/peter/plugins/pairs.lua
@@ -4,7 +4,6 @@ return {
     event = "InsertEnter",
     opts = {
       check_ts = true,
-      disable_filetype = { "snacks_picker_input" },
     },
     config = function(_, opts)
       require("nvim-autopairs").setup(opts)
