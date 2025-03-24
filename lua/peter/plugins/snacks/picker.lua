31b39588bdb313da2ee4f353b2798f2a7cbcff5f
Peter Sheehan <petersheehan2003@gmail.com>
2025-03-24 20:02:55 +0000

    refactor(snacks.picker): tidy up tables and class defn in util.snacks.picker
    Also move custom preview function into util file.

--- lua/peter/plugins/snacks/picker.lua
+++ lua/peter/plugins/snacks/picker.lua
@@ -2,6 +2,10 @@
 
 ---@module "snacks"
 
+local constants = require("peter.constants")
+local paths = constants.paths
+local picker = require("peter.util.snacks.picker")
+
 ---@type snacks.picker.Config
 local Config = {
   enabled = true,
@@ -21,144 +25,7 @@ local Config = {
   -- },
   --
 
-  preview = function(ctx)
-    local uv = vim.uv or vim.loop
-    local preview = require("snacks.picker.preview")
-
-    local patterns = require("peter.util.patterns")
-    local get_sensitive_message = patterns.get_sensitive_message
-
-    if
-      ctx.item.buf
-      and not ctx.item.file
-      and not vim.api.nvim_buf_is_valid(ctx.item.buf)
-    then
-      ctx.preview:notify("Buffer no longer exists", "error")
-      return
-    end
-
-    -- used by some LSP servers that load buffers with custom URIs
-    if
-      ctx.item.buf
-      and vim.uri_from_bufnr(ctx.item.buf):sub(1, 4) ~= "file"
-    then
-      vim.fn.bufload(ctx.item.buf)
-    elseif ctx.item.file and ctx.item.file:find("^%w+://") then
-      ctx.item.buf = vim.fn.bufadd(ctx.item.file)
-      vim.fn.bufload(ctx.item.buf)
-    end
-
-    if ctx.item.buf and vim.api.nvim_buf_is_loaded(ctx.item.buf) then
-      local name = vim.api.nvim_buf_get_name(ctx.item.buf)
-      name = uv.fs_stat(name) and vim.fn.fnamemodify(name, ":t") or name
-      ctx.preview:set_title(name)
-      ctx.preview:set_buf(ctx.item.buf)
-    else
-      local path = Snacks.picker.util.path(ctx.item)
-      if not path then
-        ctx.preview:notify("Item has no `file`", "error")
-        return
-      end
-
-      if Snacks.image.supports_file(path) then
-        return preview.image(ctx)
-      end
-
-      -- re-use existing preview when path is the same
-      if path ~= Snacks.picker.util.path(ctx.prev) then
-        ctx.preview:reset()
-        vim.bo[ctx.buf].buftype = ""
-
-        local name = vim.fn.fnamemodify(path, ":t")
-        ctx.preview:set_title(ctx.item.title or name)
-
-        local stat = uv.fs_stat(path)
-        if not stat then
-          ctx.preview:notify("file not found: " .. path, "error")
-          return false
-        end
-        if stat.type == "directory" then
-          return preview.directory(ctx)
-        end
-        local max_size = ctx.picker.opts.previewers.file.max_size
-          or (1024 * 1024)
-        if stat.size > max_size then
-          ctx.preview:notify("large file > 1MB", "warn")
-          return false
-        end
-        if stat.size == 0 then
-          ctx.preview:notify("empty file", "warn")
-          return false
-        end
-
-        local file = assert(io.open(path, "r"))
-
-        local is_binary = false
-        local ft = ctx.picker.opts.previewers.file.ft
-          or vim.filetype.match({ filename = path })
-        if ft == "bigfile" then
-          ft = nil
-        end
-        local lines = {}
-        for line in file:lines() do
-          ---@cast line string
-          if #line > ctx.picker.opts.previewers.file.max_line_length then
-            line = line:sub(1, ctx.picker.opts.previewers.file.max_line_length)
-              .. "..."
-          end
-          -- Check for binary data in the current line
-          if line:find("[%z\1-\8\11\12\14-\31]") then
-            is_binary = true
-            if not ft then
-              ctx.preview:notify("binary file", "warn")
-              return
-            end
-          end
-          table.insert(lines, line)
-        end
-
-        file:close()
-
-        if is_binary then
-          ctx.preview:wo({
-            number = false,
-            relativenumber = false,
-            cursorline = false,
-            signcolumn = "no",
-          })
-        end
-
-        -- NOTE: custom logic to not show sensitive files
-
-        ft = ctx.picker.opts.previewers.file.ft
-
-        if vim.g.private_mode_enabled then
-          local message = get_sensitive_message(path)
-          if message then
-            lines = { message }
-            ctx.preview:wo({
-              number = false,
-              relativenumber = false,
-              cursorline = false,
-              foldcolumn = "0",
-              signcolumn = "no",
-              statuscolumn = "",
-            })
-            ft = "text"
-          end
-        end
-
-        ctx.preview:set_lines(lines)
-
-        ctx.preview:highlight({
-          file = path,
-          ft = ft,
-          buf = ctx.buf,
-        })
-      end
-    end
-    ctx.preview:loc()
-  end,
+  preview = picker.preview.file,
 
   win = {
     preview = {
@@ -218,11 +85,6 @@ local pick = function(source, opts)
   end
 end
 
-local constants = require("peter.constants")
-local paths = constants.paths
-
-local pickers = require("peter.util.snacks.pickers")
-
 return {
   {
     "folke/snacks.nvim",
@@ -246,7 +108,7 @@ return {
 
       -- find
       { "<leader>ff", pick("files"), desc = "Files" },
-      { "<leader>fF", pickers.file.all_files, desc = "All Files" },
+      { "<leader>fF", picker.file.all_files, desc = "All Files" },
       { "<leader>fr", pick("recent"), desc = "Recent" },
       { "<leader>fb", pick("buffers"), desc = "Buffers" },
 
@@ -255,14 +117,14 @@ return {
       { "<leader>sB", pick("grep_buffers"), desc = "Grep Open Buffers" },
       { "<leader>sg", pick("grep"), desc = "Grep" },
       { "<leader>sk", pick("keymaps"), desc = "Keymaps" },
-      { "<leader>ss", pickers.search.lsp_symbols, desc = "LSP Symbols" },
+      { "<leader>ss", picker.search.lsp_symbols, desc = "LSP Symbols" },
 
-      { "<leader>uC", pickers.neovim.colorschemes, desc = "Colorschemes" },
+      { "<leader>uC", picker.neovim.colorschemes, desc = "Colorschemes" },
 
       -- neovim pickers
-      { "<leader>nf", pickers.neovim.config_files, desc = "Config" },
-      { "<leader>np", pickers.neovim.plugins, desc = "Plugins" },
-      { "<leader>nl", pickers.neovim.languages, desc = "Languages" },
+      { "<leader>nf", picker.neovim.config_files, desc = "Config" },
+      { "<leader>np", picker.neovim.plugins, desc = "Plugins" },
+      { "<leader>nl", picker.neovim.languages, desc = "Languages" },
     },
   },
 }
