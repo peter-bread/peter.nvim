4e90f165acda6a63b75e601c1192d79099a732c3
Peter Sheehan <petersheehan2003@gmail.com>
2025-05-10 01:48:18 +0100

    refactor(util.diagnostic): use hidden local module

--- lua/peter/util/diagnostic.lua
+++ lua/peter/util/diagnostic.lua
@@ -4,6 +4,10 @@
 ---@field prev fun(opts:vim.diagnostic.JumpOpts):(vim.Diagnostic?) Move to previous diagnostic.
 local M = {}
 
+---@class peter.util.diagnostic.hidden
+---@field jump_opts fun(count:integer, opts?:vim.diagnostic.JumpOpts):vim.diagnostic.JumpOpts Set `count` separately to rest of `opts`.
+local H = {}
+
 ---Move to a diagnostic.
 ---
 ---This is just a wrapper around the builtin
@@ -18,14 +22,23 @@ end
 ---@param opts? vim.diagnostic.JumpOpts
 ---@return (vim.Diagnostic)?
 function M.next(opts)
-  return M.jump(vim.tbl_extend("force", { count = 1 }, opts or {}))
+  return M.jump(H.jump_opts(1, opts))
 end
 
 ---Move to previous diagnostic.
 ---@param opts? vim.diagnostic.JumpOpts
 ---@return (vim.Diagnostic)?
 function M.prev(opts)
-  return M.jump(vim.tbl_extend("force", { count = -1 }, opts or {}))
+  return M.jump(H.jump_opts(-1, opts))
+end
+
+---Set `JumpOpts` for `vim.diagnostic.jump`.
+---Set `count` separately from the rest of `opts`.
+---@param count integer Number of diagnostics to move by.
+---@param opts? vim.diagnostic.JumpOpts
+---@return vim.diagnostic.JumpOpts
+function H.jump_opts(count, opts)
+  return vim.tbl_extend("force", { count = count }, opts or {})
 end
 
 return M
