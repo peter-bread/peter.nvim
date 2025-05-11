6885b693e0d9c385a5f75dda93515d8a1c1a46c2
Peter Sheehan <petersheehan2003@gmail.com>
2025-05-11 13:47:59 +0100

    refactor(options): custom format

--- lua/peter/config/options.lua
+++ lua/peter/config/options.lua
@@ -1,11 +1,13 @@
+-- stylua: ignore start
+
 -- globals =====================================================================
 
--- leader and local leader
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader      = " "      -- leader
+vim.g.maplocalleader = "\\"     -- local leader
 
--- fix markdown indentation settings
-vim.g.markdown_recommended_style = 0
+vim.g.have_nerd_font = true     -- used for plugins that might need icons
+
+vim.g.markdown_recommended_style = 0 -- fix markdown indentation settings
 
 -- options =====================================================================
 
@@ -14,92 +16,82 @@ vim.g.markdown_recommended_style = 0
 -- https://github.com/neovim/neovim/issues/20107
 
 -- line numbers ----------------------------------------------------------------
--- stylua: ignore start
-vim.o.number = true           -- line numbers
-vim.o.relativenumber = true   -- relative line numbers
--- stylua: ignore end
+
+vim.o.number          = true      -- line numbers
+vim.o.relativenumber  = true      -- relative line numbers
+
 
 -- tabs and indentation --------------------------------------------------------
--- stylua: ignore start
-vim.o.autoindent = true       -- copy indent from current line when starting a new line
-vim.o.smartindent = true      -- do smart autoindenting when starting a new line
-vim.o.expandtab = true        -- use spaces instead of tabs
-vim.o.shiftwidth = 2          -- size of indent
-vim.o.tabstop = 2             -- number of spaces tabs count for
-vim.o.smarttab = true         -- a <Tab> in front of a line inserts blanks according to 'shiftwidth'
-vim.o.shiftround = true       -- round indent
--- stylua: ignore end
+
+vim.o.autoindent      = true      -- copy indent from current line when starting a new line
+vim.o.smartindent     = true      -- do smart autoindenting when starting a new line
+vim.o.expandtab       = true      -- use spaces instead of tabs
+vim.o.shiftwidth      = 2         -- size of indent
+vim.o.tabstop         = 2         -- number of spaces tabs count for
+vim.o.smarttab        = true      -- a <Tab> in front of a line inserts blanks according to 'shiftwidth'
+vim.o.shiftround      = true      -- round indent
+
 
 -- search options --------------------------------------------------------------
--- stylua: ignore start
-vim.o.ignorecase = true       -- ignore case while searching
-vim.o.smartcase = true        -- override the 'ignorecase' option if the search pattern contains uppercase characters
-vim.o.hlsearch = true         -- highlight search matches
-vim.o.incsearch = true        -- highlight search matches while typing search command
--- stylua: ignore end
+
+vim.o.ignorecase      = true      -- ignore case while searching
+vim.o.smartcase       = true      -- override the 'ignorecase' option if the search pattern contains uppercase characters
+vim.o.hlsearch        = true      -- highlight search matches
+vim.o.incsearch       = true      -- highlight search matches while typing search command
+
 
 -- split windows ---------------------------------------------------------------
--- stylua: ignore start
-vim.o.splitright = true       -- vertical splits open on the right
-vim.o.splitbelow = true       -- horizontal splits open below
--- stylua: ignore end
+
+vim.o.splitright      = true      -- vertical splits open on the right
+vim.o.splitbelow      = true      -- horizontal splits open below
+
 
 -- appearance ------------------------------------------------------------------
 -- basic UI layout
--- stylua: ignore start
-vim.o.wrap = false            -- do not wrap lines
-vim.o.breakindent = true      -- wrapped text remains indented
-vim.o.scrolloff = 8           -- minimal number of screen lines to keep above and below the cursor
-vim.o.signcolumn = "yes"      -- always draw the signcolumn
--- line number/relativenumber display
-vim.o.statuscolumn = "%=%{v:relnum == 0 ? v:lnum : v:relnum} %s"
--- stylua: ignore end
+vim.o.wrap            = false     -- do not wrap lines
+vim.o.breakindent     = true      -- wrapped text remains indented
+vim.o.scrolloff       = 8         -- minimal number of screen lines to keep above and below the cursor
+vim.o.signcolumn      = "yes"     -- always draw the signcolumn
+
+vim.o.statuscolumn    = "%=%{v:relnum == 0 ? v:lnum : v:relnum} %s"
 
 -- visual enhancements
--- stylua: ignore start
-vim.o.cursorline = true       -- highlight current line
-vim.o.colorcolumn = "80"      -- highlight column 80
-vim.o.termguicolors = true    -- enable 24-bit RGB
-vim.o.background = "dark"     -- use dark background
--- stylua: ignore end
+vim.o.cursorline      = true      -- highlight current line
+vim.o.colorcolumn     = "80"      -- highlight column 80
+vim.o.termguicolors   = true      -- enable 24-bit RGB
+vim.o.background      = "dark"    -- use dark background
 
 -- completion/menu UI
--- stylua: ignore
-vim.o.pumheight = 15          -- max height of pop-up menus
+vim.o.pumheight       = 15        -- max height of pop-up menus
 
 -- mode/status UI
 -- TODO: set to false once there is a statusline
--- vim.o.showmode = false     -- do not show current mode (not needed with statusline)
+vim.o.showmode        = true      -- do not show current mode (not needed with statusline)
 
 -- invisible characters
--- stylua: ignore start
-vim.o.list = true             -- show invisible characters (e.g. trailing spaces)
-vim.opt.listchars = {         -- configure how invisible characters should be shown
-  nbsp = "+",
-  eol = "󱞥",
-  trail = "·",
-  tab = "  ",
-}
--- stylua: ignore end
+vim.o.list            = true      -- show invisible characters (e.g. trailing spaces)
+vim.opt.listchars     = {         -- configure how invisible characters should be shown
+                          nbsp  = "+",
+                          eol   = "󱞥",
+                          trail = "·",
+                          tab   = "  ",
+                        }
 
 -- other -----------------------------------------------------------------------
 
--- stylua: ignore
-vim.o.inccommand = "split"    -- shows the effects of commands as you type (e.g. |:substitute|)
+vim.o.inccommand      = "split"   -- shows the effects of commands as you type (e.g. |:substitute|)
 
 -- TODO: set up proper spell-checking
--- allow british and american english
--- stylua: ignore
-vim.opt.spelllang = { "en_gb", "en_us" }
+vim.opt.spelllang     = {         -- allow british and american english
+                          "en_gb",
+                          "en_us"
+                        }
 
--- stylua: ignore start
-vim.o.undofile = true         -- save undo history into a file
-vim.o.timeoutlen = 300        -- time in milliseconds to wait for a mapped sequence to complete
-vim.o.confirm = true          -- confirm saved changes before exiting buffer
--- stylua: ignore end
-
--- vim.o.exrc = true          -- allow project-local config (security risk)
+vim.o.undofile        = true      -- save undo history into a file
+vim.o.timeoutlen      = 300       -- time in milliseconds to wait for a mapped sequence to complete
+vim.o.confirm         = true      -- confirm saved changes before exiting buffer
 
 -- backspace over autoindent, line breaks and start of insert
--- stylua: ignore
-vim.o.backspace = "indent,eol,start"
+vim.o.backspace       = "indent,eol,start"
+
+-- stylua: ignore end
