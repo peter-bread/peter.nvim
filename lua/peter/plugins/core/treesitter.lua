629b9b4486db0542b636a6de52b56a9c4fc2b013
Peter Sheehan <petersheehan2003@gmail.com>
2025-05-25 01:52:15 +0100

    feat(plguins.core.treesitter)!: migrate to main branch

--- lua/peter/plugins/core/treesitter.lua
+++ lua/peter/plugins/core/treesitter.lua
@@ -8,36 +8,40 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-    cmd = { "TSUpdate", "TSUpdateSync", "TSInstall" },
+    lazy = false, -- plugin does *not* support lazy-loading
+    branch = "main", -- eventually this will become the default branch
     build = ":TSUpdate",
-    opts_extend = { "ensure_installed" },
+    opts_extend = { "custom.ensure_installed" },
 
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
+    ---@type {plugin:TSConfig,custom:table}
     opts = {
-      ensure_installed = {
-        -- INFO: these should ALWAYS be installed
-        "c",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "vim",
-        "vimdoc",
-
-        -- miscellaneous parsers that do not fit into a "language" easily
-        "regex",
+      ---@type TSConfig Actual config for setup funcion
+      plugin = {
+        install_dir = vim.fn.stdpath("data") .. "/site", -- default value
       },
 
-      auto_install = false,
+      ---@type table Additional data for custom config, i.e. installing parsers
+      custom = {
+        ensure_installed = {
+          -- INFO: these should ALWAYS be installed
+          "c",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "query",
+          "vim",
+          "vimdoc",
 
-      -- TODO: other config
+          -- miscellaneous parsers that do not fit into a "language" easily
+          "regex",
+        },
+      },
     },
+
     config = function(_, opts)
-      local list = require("peter.util.list")
-      opts.ensure_installed = list.uniq(opts.ensure_installed or {})
-      require("nvim-treesitter.configs").setup(opts)
+      local ts = require("nvim-treesitter")
+      ts.setup(opts.plugin)
+      ts.install(opts.custom.ensure_installed)
     end,
   },
 }
