8b922c68953a9e49c19a8131bb44b9c657c9cbd6
Peter Sheehan <petersheehan2003@gmail.com>
2025-05-30 13:02:57 +0100

    chore(util.debug): add annotations

--- lua/peter/util/debug.lua
+++ lua/peter/util/debug.lua
@@ -1,5 +1,9 @@
+---@class peter.util.debug
+---@field pp fun(...) Pretty-print any value.
 local M = {}
 
+---Pretty-print any value.
+---@param ... any Value to print.
 function M.pp(...)
   print(vim.inspect(...))
 end
