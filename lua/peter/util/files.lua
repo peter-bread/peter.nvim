c3e470e17cf7ee8671149c6b67e4a8ebe956ac48
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-03 22:59:00 +0100

    feat(util.files): add `has_extension` and redo docs

--- lua/peter/util/files.lua
+++ lua/peter/util/files.lua
@@ -1,17 +1,15 @@
 ---@class peter.util.files
----@field strip_extension fun(filename:string,extension?:string):string
+---@field strip_extension fun(filename:string,extension?:string):string Remove extension from filename.
+---@field get_extension fun(filename:string,extension?:string):string|nil Get extension from filename.
 local M = {}
 
----@param filename string
----@param extension? string
----@return string
+---Remove extension from filename.
 ---
---- Remove extension from filename.
---- If `extension` is given, only remove that extension.
---- If the filename does not contain an extension, or does not contain
---- the given `extention`, the function will just return `filename`.
+---If `extension` is given, only remove that extension.
+---If the filename does not contain an extension, or does not contain
+---the given `extention`, the function will just return `filename`.
 ---
---- # Usage
+---### Usage
 ---
 ---```lua
 --- strip_extension("hello.lua") -- "hello"
@@ -19,11 +17,44 @@ local M = {}
 ---
 --- strip_extension("hello.lua", "lua") -- "hello"
 --- strip_extension("bye.toml", "lua") -- "bye.toml"
---- ```
+---```
+---@param filename string
+---@param extension? string
+---@return string
 function M.strip_extension(filename, extension)
   local pattern = extension and ("%." .. extension .. "$") or "%.%w+$"
   local new_filename = filename:gsub(pattern, "")
   return new_filename
+end
+
+---Get extension from filename.
+---
+---If called with just `filename`, check for any extension. If found, return it.
+---Else return `nil`.
+---
+---If `extension` is provided, only check for that extension.
+---
+---Since any `string` is "truthy" and `nil` is "falsy", this function can also
+---be used to check if a filename has an extension.
+---
+---### Usage
+---
+---```lua
+--- if has_extension("foo.lua") then
+---   -- returns "lua" so does enter the conditonal block
+--- done
+---
+--- if has_extension("bar.toml", "lua") then
+---   -- returns nil, so does NOT enter the conditonal block.
+--- done
+---```
+---@param filename string
+---@param extension? string
+---@return string|nil
+function M.get_extension(filename, extension)
+  -- use () to capture extension without dot prefix
+  local pattern = extension and ("%.(" .. extension .. ")$") or "%.(%w+)$"
+  return filename:match(pattern)
 end
 
 return M
