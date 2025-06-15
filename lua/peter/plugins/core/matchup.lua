c09ba1815bfc1cfdd07a404d2947e58452800f4a
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-15 01:26:40 +0100

    fix(vim-matchup): uses fork to work with treesitter main branch
    Currently the plugin depends on functions from the master branch of treesitter.
    Using a fork until it gets merged into the actual plugin.

--- lua/peter/plugins/core/matchup.lua
+++ lua/peter/plugins/core/matchup.lua
@@ -6,7 +6,21 @@
 ---@type LazyPluginSpec[]
 return {
   {
-    "andymass/vim-matchup",
+    -- TODO: revert to main plugin once treesitter PR is merged. See below.
+    -- "andymass/vim-matchup",
+
+    -- HACK: `vim-matchup` still depends on nvim-treesitter#master.
+    -- This fork has removed that dependency.
+    -- See:
+    -- - https://github.com/andymass/vim-matchup/pull/390
+    -- - https://github.com/andymass/vim-matchup/pull/330
+    url = "https://github.com/TheLeoP/vim-matchup",
+    branch = "update-treesitter",
+
     event = { "BufReadPost", "BufNewFile" },
+
+    init = function()
+      vim.g.matchup_treesitter_enabled = true
+    end
   },
 }
