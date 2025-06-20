e94fb081f239e1ae59587e19fb78c22d1956fc4a
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-20 15:36:52 +0100

    feat(icons): adds icons module

---
+++ lua/peter/util/icons.lua
@@ -0,0 +1,25 @@
+-- stylua: ignore
+---@class peter.util.icons
+local icons = {
+
+  diagnostics = {
+    Error = " ",
+    Warn  = " ",
+    Hint  = " ",
+    -- Hint  = " ",
+    Info  = " ",
+  },
+
+  git = {
+    added     = " ",
+    modified  = " ",
+    removed   = " ",
+
+    ignored = " ",
+    renamed = " ",
+  },
+
+  kinds = {},
+}
+
+return icons
