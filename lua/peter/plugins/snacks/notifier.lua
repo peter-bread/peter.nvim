9b04aafaa4271ed286450a16da20043e28211267
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-20 15:46:12 +0100

    feat(snacks): adds notifier

---
+++ lua/peter/plugins/snacks/notifier.lua
@@ -0,0 +1,16 @@
+---@module "snacks"
+
+local P = require("peter.util.plugins.plugins")
+
+---@type snacks.notifier.Config
+---@diagnostic disable-next-line: missing-fields
+local cfg = {
+  enabled = true,
+  timeout = 5000,
+  style = "compact",
+  sort = { "added" },
+}
+
+return {
+  P.snacks({ notifier = cfg })
+}
