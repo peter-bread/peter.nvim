fc226cf6ddab1ee98a03ee06b0dbe4ba21202c06
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-21 02:04:36 +0100

    fix(ftplugin): adds utility module needed for ftplugin autocmd creation

---
+++ lua/peter/util/strings.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+---Capitalise a word.
+---First letter will be uppercase, all others lowercase.
+---@param word string
+---@return string
+function M.capitalise(word)
+  if type(word) ~= "string" then
+    return ""
+  end
+  return word:sub(1, 1):upper() .. word:sub(2):lower()
+end
+
+return M
