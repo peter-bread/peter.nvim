f65450644bec2a8e0b4a3cbba28d21d8019077ec
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-21 02:05:52 +0100

    feat(languages): adds basic lua support

---
+++ lua/peter/languages/lua.lua
@@ -0,0 +1,11 @@
+local L = require("peter.util.plugins.languages")
+
+---@type peter.lang.config
+return {
+  lsp = { "lua_ls" },
+
+  plugins = {
+    L.treesitter({ "lua", "luadoc" }),
+    L.mason({ "lua_ls", "stylua" }),
+  },
+}
