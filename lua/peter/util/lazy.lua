ebe881d69588b10cd7b69b3617dfd1e615d5be0b
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-22 23:19:30 +0100

    feat(lazy): adds utility functions
    - is_loaded: check if a plugin exists in the spec and has been loaded
    - on_load: run a function once a plugin has loaded

---
+++ lua/peter/util/lazy.lua
@@ -0,0 +1,32 @@
+---@class peter.util.lazy
+local M = {}
+
+-- What is the difference between these?
+-- local x = require("lazy.core.config").plugins[plugin]
+-- local y = require("lazy.core.config").spec.plugins[plugin]
+
+function M.is_loaded(plugin)
+  local config = require("lazy.core.config")
+  return config.plugins[plugin] and config.plugins[plugin]._.loaded
+end
+
+---Run `fn` if `name` is already loaded or when it loads.
+---@param name string
+---@param fn function
+function M.on_load(name, fn)
+  if M.is_loaded(name) then
+    fn()
+  else
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyLoad",
+      callback = function(ev)
+        if ev.data == name then
+          fn()
+          return true
+        end
+      end,
+    })
+  end
+end
+
+return M
