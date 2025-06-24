e2462fc9b21e13e36f13576c649b56fa8fd549a1
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-24 13:58:41 +0100

    fix(matchup): update with extra lua query

--- lua/peter/plugins/core/matchup.lua
+++ lua/peter/plugins/core/matchup.lua
@@ -17,13 +17,13 @@ return {
     -- See:
     -- - https://github.com/andymass/vim-matchup/pull/390
     -- - https://github.com/andymass/vim-matchup/pull/330
+
     url = "https://github.com/TheLeoP/vim-matchup",
     branch = "update-treesitter",
 
     event = { "BufReadPost", "BufNewFile" },
 
-    init = function()
-      vim.g.matchup_treesitter_enabled = true
-    end,
+    -- set options using vim.g.matchup_<option> = <value>
+    init = function() end,
   },
 }
