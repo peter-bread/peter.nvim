53ea550ec51ae65b9792dbc7833bce30b2422c59
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-24 02:08:15 +0100

    refactor(languages): use pcall for safety

--- lua/peter/util/languages.lua
+++ lua/peter/util/languages.lua
@@ -5,7 +5,12 @@ local M = {}
 ---Run `fn` on all language configs.
 ---@param fn fun(name:string, cfg:peter.lang.config) Function to run for each language.
 function M.for_each(fn)
-  for name, cfg in pairs(require("peter.languages") or {}) do
+  local ok, languages = pcall(require, "peter.languages")
+  if not ok then
+    return
+  end
+
+  for name, cfg in pairs(languages or {}) do
     fn(name, cfg)
   end
 end
