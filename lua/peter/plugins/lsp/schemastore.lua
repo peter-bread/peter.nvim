2c05d5275c4ca46c785ca4feb0cfdd445152cb37
Peter Sheehan <petersheehan2003@gmail.com>
2025-06-30 10:55:39 +0100

    feat(lsp): adds schemastore catalog
    This will be used when json and yaml languages are properly set up.

---
+++ lua/peter/plugins/lsp/schemastore.lua
@@ -0,0 +1,12 @@
+-- See https://github.com/b0o/SchemaStore.nvim/issues/45 for how to use with
+-- vim.lsp.config
+
+-- Schemas for jsonls and yamlls
+
+---@type LazyPluginSpec[]
+return {
+  {
+    "b0o/SchemaStore.nvim",
+    lazy = true,
+  },
+}
