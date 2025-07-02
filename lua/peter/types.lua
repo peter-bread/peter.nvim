737f1b7406c5e9011551f3ccc0d99b7aa014d1eb
Peter Sheehan <petersheehan2003@gmail.com>
2025-07-03 00:01:51 +0100

    chore(types): remove old comments
    May delete this file altogether.

--- lua/peter/types.lua
+++ lua/peter/types.lua
@@ -1,12 +1,7 @@
 ---@meta
 
----@module "lazy"
-
 -- types and aliases
 -- should all be defined under the `peter` namespace
 -- broken into sections
--- if these sections get too large then they will be split into separate files
 
 -- Core ------------------------------------------------------------------------
-
--- Language --------------------------------------------------------------------
