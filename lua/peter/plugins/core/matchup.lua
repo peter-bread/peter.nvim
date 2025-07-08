23d635f06086c117986db56d2d86316b57da9274
Peter Sheehan <petersheehan2003@gmail.com>
2025-07-08 11:52:46 +0100

    feat(matchup): switch back to master branch now PR has been merged

--- lua/peter/plugins/core/matchup.lua
+++ lua/peter/plugins/core/matchup.lua
@@ -6,21 +6,7 @@
 ---@type LazyPluginSpec[]
 return {
   {
-    -- TODO: revert to main plugin once treesitter PR is merged. See below.
-    -- "andymass/vim-matchup",
-
-    -- HACK: Latest release and main branch of vim-matchup still depend on
-    -- nvim-treesitter/nvim-treesitter@master. Currently I am using main and not
-    -- master, so these plugins are incompatible.
-    --
-    -- This fork has removed that dependency.
-    -- See:
-    -- - https://github.com/andymass/vim-matchup/pull/390
-    -- - https://github.com/andymass/vim-matchup/pull/330
-
-    url = "https://github.com/TheLeoP/vim-matchup",
-    branch = "update-treesitter",
-
+    "andymass/vim-matchup",
     event = { "BufReadPost", "BufNewFile" },
 
     -- set options using vim.g.matchup_<option> = <value>
