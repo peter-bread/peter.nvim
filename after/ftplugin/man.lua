77a4166344d43b9a036733553301f235788af677
Peter Sheehan <petersheehan2003@gmail.com>
2025-08-08 18:16:28 +0100

    fix(man): disable statuscolumn for manpages + replace gO keymap w new desc

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -1,3 +1,12 @@
--- HACK: make sure relative number works in man pages
--- (This might be a bug???)
-vim.wo.relativenumber = true
+local keymap = require("peter.util.keymap")
+
+-- 'number' and 'relativenumber' are both disabled by default so no need for
+-- statuscolum.
+vim.wo.statuscolumn = ""
+
+keymap.safe_del("n", "gO", { buffer = true })
+
+vim.keymap.set("n", "gO", require("man").show_toc, {
+  desc = "Table of Contents",
+  buffer = true,
+})
---
+++ lua/peter/util/keymap.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+---Safely remove an existing mapping.
+---@param modes string|string[]
+---@param lhs string
+---@param opts? vim.keymap.del.Opts
+function M.safe_del(modes, lhs, opts)
+  pcall(function()
+    vim.keymap.del(modes, lhs, opts)
+  end)
+end
+
+return M
