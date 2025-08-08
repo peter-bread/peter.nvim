b0c083d62b54f743b678b4bc2c483eb8a7d0aefd
Peter Sheehan <petersheehan2003@gmail.com>
2025-08-08 18:16:56 +0100

    fix(help): disable statuscolumn

--- after/ftplugin/help.lua
+++ after/ftplugin/help.lua
@@ -1,6 +1,8 @@
 -- smaller scrolloff as help usually viewed in a horizontal split
 vim.wo.scrolloff = 4
 
+vim.wo.statuscolumn = ""
+
 -- use `q` to close help page
 vim.keymap.set("n", "q", "<cmd>q<cr>", {
   desc = "Quit",
