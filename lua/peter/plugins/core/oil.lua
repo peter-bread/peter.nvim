f580b5033aa005343f7c7fb2536bebbac08a38b9
Peter Sheehan <petersheehan2003@gmail.com>
2025-08-12 14:10:09 +0100

    chore(types): adds type annotations for oil setup opts

--- lua/peter/plugins/core/oil.lua
+++ lua/peter/plugins/core/oil.lua
@@ -1,10 +1,12 @@
 ---@module "lazy"
+---@module "oil"
 
 ---@type LazyPluginSpec[]
 return {
   {
     "stevearc/oil.nvim",
     keys = { { "-", "<cmd>Oil<cr>", desc = "Oil" } },
+    ---@type oil.SetupOpts
     opts = {
       columns = { "icon", "permissions", "size", "mtime" },
       skip_confirm_for_simple_edits = true,
