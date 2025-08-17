b29294ae0bebf18383d3cbe7fda74e4c22b63772
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-08-17 01:16:10 +0100

    feat(python): (WIP) initial python config

---
+++ lua/peter/languages/python.lua
@@ -0,0 +1,29 @@
+---@module "conform"
+
+local L = require("peter.util.plugins.languages")
+
+---@type peter.lang.config
+return {
+  lsp = { "basedpyright", "ruff" },
+
+  plugins = {
+    L.treesitter({
+      "python",
+      -- "pymanifest"
+    }),
+
+    L.mason({
+      "basedpyright",
+      "ruff",
+    }),
+
+    L.format({
+      python = { "ruff_organize_imports", "ruff_format" },
+      -- We *could* use the following:
+      -- python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
+      -- However, I think it's better to make actual code changes manually
+      -- with intent, e.g. with an LSP Code Action (see `:h vim.lsp.buf.code_action()`),
+      -- rather than with a formatter that runs on every write.
+    }),
+  },
+}
