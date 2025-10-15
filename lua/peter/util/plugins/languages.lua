bad7dbda71ba845d130754997da5a9919a162693
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-10-15 18:06:32 +0100

    fix(languages): correct type for mason-tool-installer packages

--- lua/peter/util/plugins/languages.lua
+++ lua/peter/util/plugins/languages.lua
@@ -1,7 +1,7 @@
 ---@module "lazy"
 ---@module "conform"
 
----@alias peter.lang.masonPackage string|{name:string, version?:string, condition?:fun():boolean}
+---@alias peter.lang.masonPackage string|{[1]:string, version?:string, auto_update?:boolean, condition?:fun():boolean}
 ---@alias peter.lang.formatters_by_ft table<string, conform.FiletypeFormatterInternal|fun(bufnr: integer):conform.FiletypeFormatterInternal>
 
 ---@class peter.util.plugins.lang
@@ -31,6 +31,18 @@ end
 
 ---Add mason packages to be installed.
 ---Plugin: [mason.nvim](https://github.com/mason-org/mason.nvim).
+---
+---Example:
+---
+--- ```lua
+--- -- Standard
+--- L.mason({ "gopls" })
+---
+--- -- Additional options
+--- L.mason({ "gopls" , version = "v0.19.1", auto_update = false, condition = function()
+---   return vim.fn.executable("go") == 1
+--- end })
+--- ```
 ---@param packages peter.lang.masonPackage[] List of packages to install.
 ---@return LazyPluginSpec
 function M.mason(packages)
