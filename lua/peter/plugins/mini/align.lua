0de2ae320b9b125ee8adab4b142c1ef6dc6b4f54
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2025-12-23 01:58:41 +0000

    feat(align): adds mini.align

---
+++ lua/peter/plugins/mini/align.lua
@@ -0,0 +1,12 @@
+---@module "lazy"
+
+-- Align text interactively.
+-- See 'https://github.com/nvim-mini/mini.align'.
+
+---@type LazyPluginSpec[]
+return {
+  {
+    "nvim-mini/mini.align",
+    opts = {},
+  },
+}
