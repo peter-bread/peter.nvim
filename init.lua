675a8ed2001c69d8d28defb7e0b640a0d1074388
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-01-16 19:37:50 +0000

    fix: don't load config if neovim version is too old

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
+if vim.fn.has("nvim-0.11") ~= 1 then
+  vim.notify_once(
+    "peter.nvim requires Neovim 0.11 or above. Current version: "
+      .. require("peter.util.version").string()
+      .. ". Upgrade if possible. "
+      .. "\n"
+      .. "Altervatively, checkout an older tagged version of the config (not recommended).",
+    vim.log.levels.ERROR
+  )
+  return
+end
+
 require("peter.config")
