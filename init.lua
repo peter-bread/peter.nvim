60d417ea019076a866d022f9e3a967c433e731cc
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-01-16 19:41:02 +0000

    refactor: slightly modify message

--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
 if vim.fn.has("nvim-0.11") ~= 1 then
   vim.notify_once(
-    "peter.nvim requires Neovim 0.11 or above. Current version: "
+    "peter.nvim requires Neovim 0.11 or above. "
+      .. "Current version: "
       .. require("peter.util.version").string()
-      .. ". Upgrade if possible. "
+      .. ". "
+      .. "Upgrade if possible."
       .. "\n"
       .. "Altervatively, checkout an older tagged version of the config (not recommended).",
     vim.log.levels.ERROR
