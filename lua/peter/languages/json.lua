ad828c078293478f9006dd6262d53216cfcab827
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-02-20 19:17:04 +0000

    feat(json): register formatter for jsonc

--- lua/peter/languages/json.lua
+++ lua/peter/languages/json.lua
@@ -7,6 +7,12 @@ return {
   plugins = {
     treesitter = { "json", "json5" },
     mason = { "jsonls", "prettier" },
-    format = { json = { "prettier" } },
+    format = {
+      json = { "prettier" },
+      -- Prettier will probably need to be configured for JSONC formatting on a per-project basis.
+      -- See 'https://github.com/peter-bread/.dotfiles/commit/176620f25661c8a717fc5da676e6dbdaff43872b'
+      -- for an example.
+      jsonc = { "prettier" },
+    },
   },
 }
