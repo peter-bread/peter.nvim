ee04eadf2d338539067032baaeb584ee3eb5a46b
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-02-24 20:15:32 +0000

    chore(rust): adds comment and TODO about rust lsp/fmt installation

--- lua/peter/languages/rust.lua
+++ lua/peter/languages/rust.lua
@@ -1,5 +1,8 @@
 -- See 'https://rust-lang.org/'.
 
+-- Assumes rust-analyzer and rustfmt are installed via rustup.
+-- TODO: Add links or snippets to this.
+
 ---@type peter.lang.Config
 return {
   lsp = { "rust_analyzer" },
