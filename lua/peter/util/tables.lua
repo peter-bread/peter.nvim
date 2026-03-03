d9b31e58dc56e071cedab53960ead90412ed262b
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-03-03 02:01:09 +0000

    fix(tables): correct behaviour when removing keys from a table
    There are now two functions:
    - Remove keys in-place,
    - Remove keys and return a copy (subject to remove).
    
    Fixes #152.

--- lua/peter/util/tables.lua
+++ lua/peter/util/tables.lua
@@ -1,24 +1,50 @@
 ---@class peter.util.table
 local M = {}
 
+local H = {}
+
 --[[ ---------------------------------------------------------------------- ]]
 --
 --[[ ------------------- START OF PUBLIC API FUNCTIONS. ------------------- ]]
 --
 --[[ ---------------------------------------------------------------------- ]]
 
----Return a copy of `tbl` with `keys` removed.
----@param tbl table Original table.
----@param keys string[] Keys to be removed.
----@return table
-function M.without(tbl, keys)
-  local ret = {}
-  for k, v in pairs(tbl) do
-    if not keys[k] then
-      ret[k] = v
-    end
+---Remove `keys` from `tbl` in-place.
+---@param tbl table Table to mutate.
+---@param keys string[] Keys to remove.
+---@return table tbl The mutated table.
+function M.without_keys(tbl, keys)
+  local keyset = H.make_keyset(keys)
+
+  for key in pairs(keyset) do
+    tbl[key] = nil
   end
-  return ret
+
+  return tbl
+end
+
+---Remove `keys` from a copy of `tbl`.
+---
+---WARNING: This function is subject to change and/or removal.
+---@param tbl table Original table.
+---@param keys string[] Keys to remove.
+---@return table tbl_copy New table with keys removed.
+function M.without_keys_copy(tbl, keys)
+  return M.without_keys(vim.deepcopy(tbl), keys)
+end
+
+--[[ ---------------------------------------------------------------------- ]]
+--
+--[[ ---------- END OF API FUNCTIONS. START OF HELPER FUNCTIONS. ---------- ]]
+--
+--[[ ---------------------------------------------------------------------- ]]
+
+function H.make_keyset(keys)
+  local keyset = {}
+  for _, key in ipairs(keys) do
+    keyset[key] = true
+  end
+  return keyset
 end
 
 return M
