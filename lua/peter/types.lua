725850d4b27ea6d55be192c21f190a6d12142733
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-03-15 12:00:56 +0000

    chore: delete unused types file

--- lua/peter/types.lua
+++
@@ -1,7 +0,0 @@
----@meta
-
--- types and aliases
--- should all be defined under the `peter` namespace
--- broken into sections
-
--- Core ------------------------------------------------------------------------
