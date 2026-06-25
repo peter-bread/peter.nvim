c9190066aefadb07dbba87b06ef55cfca3931e8f
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-06-25 16:42:06 +0100

    feat(oil): change keymap and initial work on shell cmd keymap
    Refs #132

--- lua/peter/plugins/core/oil.lua
+++ lua/peter/plugins/core/oil.lua
@@ -20,7 +20,34 @@ return {
         ["<C-l>"] = false,
         ["<localleader>|"] = { "actions.select", opts = { vertical = true } },
         ["<localleader>-"] = { "actions.select", opts = { horizontal = true } },
-        ["<localleader>r"] = { "actions.refresh" },
+        ["<C-r>"] = { "actions.refresh" },
+
+        -- TODO: Run shell command with current oil item as argument.
+        -- ["<C-x>"] = function()
+        --   local oil = require("oil")
+        --
+        --   local dir = oil.get_current_dir()
+        --
+        --   local entry = oil.get_cursor_entry()
+        --   if not entry then
+        --     return
+        --   end
+        --
+        --   local path = dir .. entry.name
+        --
+        --   vim.ui.input({ prompt = "Command: " }, function(input)
+        --     local cmd_str = input .. " " .. path
+        --     local cmd = vim.split(cmd_str, " ", { trimempty = true })
+        --
+        --     vim.system(cmd, {}, function(out)
+        --       vim.print(out.stdout)
+        --       vim.print(out.stderr)
+        --       if out.code ~= 0 then
+        --         vim.print("Exited with " .. out.code)
+        --       end
+        --     end)
+        --   end)
+        -- end,
       },
     },
   },
