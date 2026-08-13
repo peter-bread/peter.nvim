dac377c39064f4fbee0fad10d3a91b2eaaf66028
Peter Sheehan <145384599+peter-bread@users.noreply.github.com>
2026-08-13 02:19:12 +0100

    fix: update lazydev to work with newer versions of lua_ls

--- lua/peter/languages/lua.lua
+++ lua/peter/languages/lua.lua
@@ -6,19 +6,7 @@ return {
 
   plugins = {
     treesitter = { "lua", "luadoc" },
-
-    -- HACK: Pin Lua Language Server to 3.16.4.
-    -- See 'https://github.com/folke/lazydev.nvim/issues/136'.
-    --
-    -- Alternate solution, in LSP config:
-    -- settings = {
-    --   Lua = {
-    --     workspace = {
-    --       library = vim.api.nvim_get_runtime_file("", true),
-    --     },
-    --   },
-    -- }
-    mason = { { "lua_ls", version = "3.16.4" }, "stylua", "selene" },
+    mason = { "lua_ls", "stylua", "selene" },
     format = { lua = { "stylua" } },
     lint = { lua = { "selene" } },
   },
